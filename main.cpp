49806097aebcb9c61bc1777a3110332a5f34240f
Muqi <limuqi123@gmail.com>
2019-12-01 12:57:17 -0800

    Add some shortcut description

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 // Command shift B build
+// You can add additional cursors to all occurrences of the current selection with ⇧⌘L
+// If you do not want to add all occurrences of the current selection, you can use ⌘D instead.
+// To set cursors above or below the current position use: ⌥⌘↑ or ⌥⌘↓
+// To add cursors at arbitrary positions, select a position with your mouse and use Alt+Click
+// Bring up terminal: control + `
+// Column (box) selection You can select blocks of text by holding Shift+Alt 
 #include <iostream>
 #include <string>
 using namespace std;
  int main(){
      string str = "Hello World";
-     std::cout<<str<< endl;
+     std::cout<< str<< endl;
      return 0;
  } 
